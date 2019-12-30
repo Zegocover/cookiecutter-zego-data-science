0df1530ce9a7a2a1dc6f5dcae101536073b080ad
Joe Hartshorn <joe.hartshorn@zego.com>
2019-12-30 11:26:33 +0000

    Added basic dvc things

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,3 +3,11 @@
 git init
 git add .
 git commit -m "Initial commit"
+
+dvc init
+dvc remote add s3 {{ cookiecutter.s3_bucket }}/dvc
+dvc config core.remote s3
+dvc remote modify s3 profile {{ cookiecutter.aws_profile }}
+
+git add .
+git commit -m "Initial dvc commit"
